87114e494ab264f600c82c9e47e8d699e9351827
Andor Saga <andor.saga@gmail.com>
2019-06-09 18:24:48 -0400

    .

--- demo/src/viz_psysImageMapSpawn.js
+++ demo/src/viz_psysImageMapSpawn.js
@@ -5,10 +5,147 @@ let imgLoaded;
 let imageMap;
 let listed = false;
 let p;
+let WW, WH;
+let particlesPerFrame = 15;
+let imgScale = 6;
+let particles;
+
+class Particles {
+  constructor(cfg) {
+    this.timer = 0;
+
+    this.count = cfg.count;
+    this._p = createVector();
+
+    this.alive = new Uint8Array(this.count);
+    this.age = new Float32Array(this.count);
+    this.lifetime = new Float32Array(this.count);
+
+    this.pos = new Float32Array(this.count * 2);
+    this.vel = new Float32Array(this.count * 2);
+    this.acc = new Float32Array(this.count * 2);
+
+    this.col = new Float32Array(this.count * 4);
+
+    this.reset();
+  }
+
+  // is this necessary?
+  reset() {
+    for (let i = 0; i < this.count; i++) {
+      this.alive[i] = 0;
+    }
+  }
+
+  /*
+  	returns index of -1 if no free slots available
+  */
+  findDeadParticle() {
+    for (let i = 0; i < this.count; i++) {
+      if (this.particleIsAlive(i) === false) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  spawnParticle() {
+    let idx = this.findDeadParticle();
+
+    // debugger;
+    if (idx > -1) {
+      this._p = createVector();
+      let c = [0, 0, 0, 0];
+      imageMap.getRandomPoint(this._p, c);
+
+      this.col[idx * 4 + 0] = c[0];
+      this.col[idx * 4 + 1] = c[1];
+      this.col[idx * 4 + 2] = c[2];
+      this.col[idx * 4 + 3] = c[3];
+
+
+      this.pos[idx * 2 + 0] = this._p.x;
+      this.pos[idx * 2 + 1] = this._p.y;
+
+      this.age[idx] = 0;
+      this.alive[idx] = 1;
+      this.lifetime[idx] = random(0.1, 0.2);
+
+      this.vel[idx * 2 + 0] = 0;
+      this.vel[idx * 2 + 1] = random(-1, -3);
+
+
+
+      return true;
+    }
+    return false;
+  }
+
+  killParticle(i) {
+    this.alive[i] = 0;
+  }
+
+  particleIsAlive(i) {
+    return this.alive[i] === 1;
+  }
+
+  updateParticle(i, dt) {
+    if (this.particleIsAlive(i) === false) { return; }
+
+    this.age[i] += dt;
+    if (this.age[i] >= this.lifetime[i]) {
+      this.killParticle(i);
+    }
+
+    this.pos[i * 2 + 0] += this.vel[i * 2 + 0] * dt;
+    this.pos[i * 2 + 1] += this.vel[i * 2 + 1] * dt;
+
+    // this.vel[i + 0] += this.acc[0];
+    // this.vel[i + 1] += this.acc[1];
+  }
+
+  update(dt) {
+    this.timer += dt;
+
+    if (this.timer > .01) {
+      this.timer = 0;
+      for (let i = 0; i < particlesPerFrame; i++) {
+        this.spawnParticle();
+      }
+    }
+
+    for (let i = 0; i < this.count; i++) {
+      this.updateParticle(i, dt);
+    }
+  }
+
+  drawParticle(i) {
+    if (this.particleIsAlive(i) === false) { return; }
+
+    let x = this.pos[i * 2 + 0] * imgScale;
+    let y = this.pos[i * 2 + 1] * imgScale;
+
+    fill(this.col[i * 4 + 0],
+      this.col[i * 4 + 1],
+      this.col[i * 4 + 2],
+      this.col[i * 4 + 3]);
+    
+    ellipse(x,y, imgScale, imgScale);
+    // rect(x, y, imgScale, imgScale);
+  }
+
+  draw() {
+    noStroke();
+    fill(255);
+
+    for (let i = 0; i < this.count; i++) {
+      this.drawParticle(i);
+    }
+  }
+}
 
 class ImageMap {
   constructor() {
-    console.log('ctor');
     this.scale = 1;
     this.indices = [];
     this.img;
@@ -34,13 +171,19 @@ class ImageMap {
     this.scale = s;
   }
 
-  getRandomPoint(p) {
+  getRandomPoint(p, c) {
     let ridx = floor(random(0, this.indices.length));
 
     // we have the index which needs to be converted into an xy pair
     let idx = this.indices[ridx];
     p.x = idx % this.img.width;
     p.y = floor(idx / this.img.width);
+
+
+    c[0] = random(0, 255);
+    c[1] = random(0, 255);
+    c[2] = 0;
+    c[3] = 255;
   }
 }
 
@@ -50,33 +193,35 @@ window.preload = function() {
 
     imageMap = new ImageMap();
     imageMap.load(_img);
-    imageMap.setScale(2);
+    imageMap.setScale(imgScale);
 
     imgLoaded = true;
   });
 }
 
 window.setup = function() {
-  // createCanvas(windowWidth, windowHeight);
-  createCanvas(400, 400);
-  background(0);
+  createCanvas(windowWidth, windowHeight);
 
-  p = createVector();
-  stroke(255);
-  noFill();
+  noSmooth();
+
+  [WW, WH] = [windowWidth, windowHeight];
+
+  particles = new Particles({ count: 10000 });
+}
+
+function update(dt) {
+  particles.update(dt);
 }
 
 window.draw = function() {
-
   if (!imgLoaded) { return; }
 
-  if (listed === false) {
-    listed = true;
-    console.log(imageMap.indices);
-  }
+  background(0);
+  update(0.016);
 
-  imageMap.getRandomPoint(p);
-  point(p.x, p.y);
-
+  // push();
+  // scale(6);
   // image(img, 0, 0);
+  // pop();
+  particles.draw();
 }
