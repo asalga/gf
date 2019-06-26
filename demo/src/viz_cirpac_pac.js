73892e9d70aa0605532c2d09e3c0e9af8e8f31fe
Andor Saga <andor.saga@gmail.com>
2019-06-25 23:52:03 -0400

    .

--- demo/src/viz_cirpac_pac.js
+++ demo/src/viz_cirpac_pac.js
@@ -1,13 +1,6 @@
 /*
   Andor Saga
   Circle packed pac man
-  
-  0 - start file
-  1 - circle pack circle
-  2 - refactor
-  3 - define theta
-  4 - write prototype to constrain
-
   - remove intersecting circles
   - animate
   - add food
@@ -15,47 +8,79 @@
   - add trails?
 */
 
-let id = 0;
-let circles = [];
+
+const MAX_CIRCLES = 5000;
+let circles = new Array(MAX_CIRCLES);
+let ccount = 0;
 
 let WW, WH;
 let bounds;
-let maxAttempts = 10;
-let maxCircles = 300;
+let maxAttempts = 20;
+let circlesPreFrame = 5000;
 
-let minSize = 2;
-let maxSize = 20;
+let minSize = 1;
+let maxSize = 2;
 
-let padding = 2;
+let padding = 3;
 let parentSize;
-let ar = 16 / 9;
 let maxViewport;
 let parent;
-let theta = 1;
+let mouthTheta = 0;
+let RAD_TO_DEG = (180 / Math.PI);
 
-function getRandomPoint(p) {
-  p.x = random(bounds.x, bounds.w);
-  p.y = random(bounds.y, bounds.h);
+window.setup = function() {
+  createCanvas(windowWidth, windowHeight);
+  smooth();
+
+  [WW, WH] = [windowWidth, windowHeight];
+  maxViewport = WW > WH ? WH : WW;
+
+  for (let i = 0; i < MAX_CIRCLES; ++i) {
+    circles[i] = new Circle();
+  }
+  reset();
 }
 
 function reset() {
-  background(0);
+  maxViewport = WH / 2 * 0.25;
+  parentSize = 100;
 
-  maxViewport = WH / 2 * 0.8;
-  parentSize = maxViewport;
-  parent = new Circle({ pos: { x: 0, y: 0 }, rad: parentSize });
+  ccount = 0;
+  parent = new Circle();
+  parent.pos = createVector();
+  parent.rad = parentSize;
 
-  circles.length = 0;
-  bounds = {
-    x: 0,
-    y: 0,
-    w: 150,
-    h: 150
-  };
+  for (let c of circles) { c.reset(); }
 }
 
-function createCircle() {
+/*
+ */
+function isCircleInsidePacman(circle, parentCircle, theta) {
+  // get theta of circle position
+  let p = createVector(circle.x, circle.y);
+
+  let circleTheta = Math.atan2(p.y, p.x);
+  circleTheta *= RAD_TO_DEG;
+
+  if (circleTheta < 0) {
+    circleTheta += 360;
+  }
+
+  if (circleTheta > (330 + mouthTheta) || circleTheta < 30 - mouthTheta) {
+  // if (circleTheta > mouthTheta) {
+    return false;
+  }
+
+  return true;
+}
+
+/*
+ */
+function requestCreateCircle() {
   let p = createVector();
+
+  let freeIndex = findFreeIndex();
+  if (freeIndex === -1) return;
 
   let shortestDistance = Infinity;
   let rad = minSize;
@@ -65,12 +90,13 @@ function createCircle() {
   // Make sure we didn't pick a point inside another circle
   while (invalidSpot && attempts < maxAttempts) {
     getRandomPointInCircle(parent, p);
-
     attempts++;
     invalidSpot = false;
 
-    for (let i = 0; i < circles.length; i++) {
-      if (pointInCircle(p, circles[i])) {
+    for (let i = 0; i < maxAttempts; i++) {
+      if (isCircleInsidePacman(p, parent, mouthTheta) === false) {
+        invalidSpot = true;
+      } else if (pointInCircle(p, circles[i])) {
         invalidSpot = true;
       }
     }
@@ -82,54 +108,84 @@ function createCircle() {
   }
 
   // If it's the first one
-  if (circles.length === 0) {
+  if (ccount === 0) {
     rad = minSize;
   } else {
     // find closest circle
-    circles.forEach(c => {
+    for (let i = 0; i < circles.length; i++) {
+      let c = circles[i];
+      if (c.alive == false) continue;
+
       let d = distToCircle(c, p);
       if (d < shortestDistance) {
         shortestDistance = d;
         rad = d;
       }
-    });
+    }
+  }
+  if (rad < minSize) {
+    return;
   }
 
-  if (rad < minSize) return;
-  rad = rad > maxSize ? maxSize : rad;
+  rad = constrain(rad, minSize, maxSize);
 
-  let c = new Circle({ pos: p, rad: rad });
-  circles.push(c);
+  let c = circles[freeIndex];
+  c.reset();
+  c.spawn();
+  c.pos.set(p);
+  c.rad = rad;
+  freeIndex++;
+  ccount++;
 }
 
-window.setup = function() {
-  createCanvas(windowWidth, windowHeight);
-  smooth();
-  background(0);
+function findFreeIndex() {
+  for (let i = 0; i < circles.length; i++) {
+    if (circles[i].alive === false) return i;
+  }
+  return -1;
+}
 
-  WW = windowWidth;
-  WH = windowHeight;
-  maxViewport = WW > WH ? WH : WW;
+function update() {
+  mouthTheta = ((sin(millis() / 1000) + 1) / 2) * 30;
+  // console.log(floor(mouthTheta));
 
-  reset();
+  circles.forEach(c => {
+    // if (c.alive === false) return;
+    // c.reset();
+    if (isCircleInsidePacman(c.pos, parent, 80) === false) {
+      c.reset();
+      
+      // background(0);
+    }
+  });
 }
 
 window.draw = function() {
+  update();
+
   background(0);
 
-  for (let i = 0; i < 10; i++) {
-    if (circles.length < maxCircles) {
-      createCircle();
-    }
+  for (let i = 0; i < 1; i++) {
+    // if (nextCircleIdx < MAX_CIRCLES_PER_PIECE) {
+    requestCreateCircle();
+    // }
   }
 
   push();
   translate(WW / 2, WH / 2);
+  stroke(255);
+  noFill();
+  line(-1000, 0, 1000, 0);
+  line(0, -10000, 0, 10000);
+  ellipse(0, 0, parentSize * 2);
 
   noFill();
   stroke(255);
-  circles.forEach(c => c.draw());
-
+  circles.forEach(c => {
+    if (c.alive) {
+      c.draw()
+    }
+  });
   pop();
 
   if (parent.rad > maxViewport) {
@@ -137,39 +193,37 @@ window.draw = function() {
   }
 }
 
-function canAddMore() {
-  return circles.length < maxCircles;
-}
-
-function mousePressed() {
-  reset();
-}
-
 class Circle {
   constructor(cfg) {
-    this.id = id;
-    id++;
-    this.pos = cfg.pos;
-    this.rad = cfg.rad;
-    this.col = 1;
-    this.dist = dist(0, 0, this.pos.x, this.pos.y);
-    this.startTime = millis() * 1000;
+    this.pos = createVector();
+    this.reset();
   }
 
-  update(dt) {
-    this.col -= dt;
-    this.col = this.col < 0 ? 0 : this.col;
+  spawn() {
+    this.alive = true;
+  }
+
+  reset() {
+    this.rad = 0;
+    this.col = 200;
+    this.alive = false;
   }
 
   draw() {
-    fill(255);
+    // if (!this.alive) return;
+
+    fill(this.col);
     let r = this.rad * 2;
     ellipse(this.pos.x, this.pos.y, r, r);
   }
 }
 
+window.mousePressed = function() {
+  // reset();
+}
+
 function getRandomPointInCircle(c, p) {
-  let v = createVector(random(-1, 1), random(-1, 1) * noise(p.x));
+  let v = createVector(random(-1, 1), random(-1, 1));
   v.normalize();
   v.mult(random(0, c.rad));
   p.x = v.x;
@@ -183,3 +237,10 @@ function distToCircle(c, pos) {
 function pointInCircle(pos, c) {
   return dist(pos.x, pos.y, c.pos.x, c.pos.y) <= c.rad;
 }
+
+let getNextId = (function() {
+  let i = 0;
+  return function() {
+    return i++;
+  }
+})();
